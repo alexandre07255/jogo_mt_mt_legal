a61848f4643ceb7bb4c3a6be6b8a67d1602fa5dc
Alexandre Amorim <alexandre07255@gmail.com>
2024-08-07 19:04:49 -0300

    Minha alma entra em combustao

--- jogo_mt_mt_legal/inputManager.cpp
+++ jogo_mt_mt_legal/inputManager.cpp
@@ -1,1 +1,40 @@
 #include "inputManager.h"
+
+
+inputManager::inputManager():
+	keyboard(new sf::Keyboard),
+	mouse(new sf::Mouse)
+{
+
+}
+
+inputManager::~inputManager()
+{
+	delete keyboard;
+	delete mouse;
+	instance = NULL;
+}
+
+inputManager* inputManager::getInstance()
+{
+	if (instance == NULL)
+		instance = new inputManager();
+	return instance;
+}
+
+const bool inputManager::isKeyPressed(sf::Keyboard::Key key) { return keyboard->isKeyPressed(key); }
+
+const bool inputManager::isUpPressed(bool player2)
+{
+
+}
+
+const bool inputManager::isDownPressed(bool player2);
+
+const bool inputManager::isRightPressed(bool player2);
+
+const bool inputManager::isLeftPressed(bool player2);
+
+const bool inputManager::isAttackPressed(bool player2);
+
+const bool inputManager::isPausePressed();
--- jogo_mt_mt_legal/inputManager.h
+++ jogo_mt_mt_legal/inputManager.h
@@ -1,10 +1,22 @@
 #pragma once
-class inputManager
-{
-	
-	static const bool isKeyPressed(key);
-	static const bool isDownPressed();
-	void update();
+#include <SFML/Window.hpp>
 
+class inputManager //Classe Singleton e Observed :3 berunyan
+{
+private:
+	static inputManager* instance;
+	sf::Keyboard* keyboard;
+	sf::Mouse* mouse;
+	inputManager();
+public:
+	~inputManager();
+	static inputManager* getInstance();
+	const bool isKeyPressed(sf::Keyboard::Key key);
+	const bool isUpPressed(bool player2);
+	const bool isDownPressed(bool player2);
+	const bool isRightPressed(bool player2);
+	const bool isLeftPressed(bool player2);
+	const bool isAttackPressed(bool player2);
+	const bool isPausePressed();
 };
 
