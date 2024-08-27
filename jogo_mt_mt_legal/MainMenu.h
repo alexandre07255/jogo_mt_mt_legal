d9ad79ce240996a9ad86bff8eea7ab9cef6499c7
Alexandre Amorim <alexandre07255@gmail.com>
2024-08-27 17:29:11 -0300

    Dog

--- jogo_mt_mt_legal/MainMenu.h
+++ jogo_mt_mt_legal/MainMenu.h
@@ -3,24 +3,27 @@
 #include <vector>
 #include <list>
 
-class MainMenu : public Scenes::Scene
+namespace Scenes
 {
-private:
-	//primeiro é my menu, segundo é start, terceiro é options e assim vai
-	vector<EntityList> versions;
-	int version;
-public:
-	MainMenu();
-	void update();
+	class MainMenu : public Scene
+	{
+	private:
+		//primeiro é my menu, segundo é start, terceiro é options e assim vai
+		vector<EntityList> versions;
+		int version;
+	public:
+		MainMenu();
+		void update();
 
-	void stackLevel1(bool player2);
-	void stackLevel2(bool player2);
+		void stackLevel1(bool player2);
+		void stackLevel2(bool player2);
 
-	void changeMainButtons();
-	void changeStartButtons();
-	void changeOptionsButtons();
+		void changeMainButtons();
+		void changeStartButtons();
+		void changeOptionsButtons();
 
-	void goBack();
-	void moreButtons(bool level2);
-};
+		void goBack();
+		void moreButtons(bool level2);
+	};
+}
 
