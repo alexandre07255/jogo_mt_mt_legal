8950f94f9894ef902ab78e6448570a34b586177e
Tiranosaro <johnwillian0609@gmail.com>
2024-08-27 15:54:41 -0300

    coisa

--- jogo_mt_mt_legal/GraphicManager.h
+++ jogo_mt_mt_legal/GraphicManager.h
@@ -2,15 +2,17 @@
 #include <SFML/Graphics.hpp>
 #include "Entity.h"
 
-class GraphicManager {
-private:
-	sf::RenderWindow* window;
-	static GraphicManager* instance;
-public:
-	GraphicManager();
-	~GraphicManager();
-	const bool isWindowOpen();
-	sf::RenderWindow* getWindow();
-	void drawBeing(Being* pE);
-	static GraphicManager* getInstance();
-};
+namespace Managers {
+	class GraphicManager {
+	private:
+		sf::RenderWindow* window;
+		static GraphicManager* instance;
+	public:
+		GraphicManager();
+		~GraphicManager();
+		const bool isWindowOpen();
+		sf::RenderWindow* getWindow();
+		void drawBeing(Being* pE);
+		static GraphicManager* getInstance();
+	};
+}
